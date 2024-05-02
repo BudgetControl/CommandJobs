d3c25dc45756a0f5d738f41b1d5335be7a58f063
Marco De Felice <marco.defelice@qwentes.it>
2024-05-02 16:17:06 +0200

    missing bootstrap

--- config/cli-commands.php
+++ config/cli-commands.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../bootstrap/app.php';
+
 $application = new \Symfony\Component\Console\Application();
 $application->add(new \Budgetcontrol\jobs\Cli\ActivatePlannedEntry());
 $application->add(new \Budgetcontrol\jobs\Cli\AddPlannedEntry());
